b921e829d60480607084672864a52be2fdfe5901
Ing. Jonathan Olier <djom202@gmail.com>
2015-09-17 11:43:40 -0500

    Init Repository

---
+++ web/admin/assets/js/config.js
@@ -0,0 +1,27 @@
+/*
+*
+*	Config.js
+*	Put here all your config
+*
+*	Authors: Jonathan Olier
+*
+*/
+
+window.config = {
+	env : "dev",
+	dev : {
+	 	baseUrl : "http://localhost/",
+	    domain : "localhost",
+	    restApi: "http://localhost/api/"
+	},
+	qa : {
+	 	baseUrl : "http://localhost/",
+	    domain : "localhost",
+	    restApi: "http://localhost/api/"
+	},
+	prod : {
+	 	baseUrl : "http://localhost/",
+	    domain : "localhost",
+	    restApi: "http://localhost/api/"
+	}
+};
---
+++ web/client/assets/js/scripts.js
@@ -0,0 +1,12 @@
+/*
+*
+*	Scripts.js
+*	Put here all your scripts
+*
+*	Authors: Jonathan Olier
+*
+*/
+
+$(function(){
+	console.log('Init App');
+});
